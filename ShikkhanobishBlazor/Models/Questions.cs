f09f31288780be65fc2d9d7ab1ab073d3ba842aa
Abid Hassan <abidhassandiba@gmail.com>
2021-10-03 14:22:07 +0600

    QuestionPage Added

---
+++ ShikkhanobishBlazor/Models/Questions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShikkhanobishBlazor.Models
+{
+    public class Questions
+    {
+        public int questionID { get; set; }
+        public int classID { get; set; }
+        public int subjectID { get; set; }
+        public int chapterID { get; set; }
+        public int topicID { get; set; }
+        public string mainQuestion { get; set; }
+        public string option1 { get; set; }
+        public string option2 { get; set; }
+        public string option3 { get; set; }
+        public string option4 { get; set; }
+        public int rightAnswer { get; set; }
+        public int noOfQs { get; set; }
+        public string Response { get; set; }
+    }
+}
